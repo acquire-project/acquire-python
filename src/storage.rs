346cf96713f351a2cfe4ffd91d6913d0eb96d459
Alan Liddell <aliddell@chanzuckerberg.com>
2023-05-02 11:38:27 -0400

    Expand StorageProperties to support chunking and compression parameters.

--- src/storage.rs
+++ src/storage.rs
@@ -28,6 +28,25 @@ pub struct StorageProperties {
 
     #[pyo3(get, set)]
     pub(crate) bytes_per_chunk: u32,
+
+    #[pyo3(get, set)]
+    pub(crate) tile_width: u32,
+
+    #[pyo3(get, set)]
+    pub(crate) tile_height: u32,
+
+    #[pyo3(get, set)]
+    pub(crate) tile_planes: u32,
+
+    #[pyo3(get, set)]
+    #[serde(default)]
+    pub(crate) compression_codec: Option<String>,
+
+    #[pyo3(get, set)]
+    pub(crate) clevel: i32,
+
+    #[pyo3(get, set)]
+    pub(crate) shuffle: i32,
 }
 
 impl_plain_old_dict!(StorageProperties);
@@ -54,12 +73,28 @@ impl TryFrom<capi::StorageProperties> for StorageProperties {
                     .to_owned(),
             )
         };
+        let codec_id = if value.compression.codec_id.nbytes == 0 {
+            None
+        } else {
+            Some(
+                unsafe { CStr::from_ptr(value.compression.codec_id.str_) }
+                    .to_str()?
+                    .to_owned(),
+            )
+        };
+
         Ok(Self {
             filename,
             first_frame_id: value.first_frame_id,
             external_metadata_json,
             pixel_scale_um: (value.pixel_scale_um.x, value.pixel_scale_um.y),
-            bytes_per_chunk: value.bytes_per_chunk,
+            bytes_per_chunk: value.chunking.bytes_per_chunk,
+            tile_width: value.chunking.tile_width,
+            tile_height: value.chunking.tile_height,
+            tile_planes: value.chunking.tile_planes,
+            compression_codec: codec_id,
+            clevel: value.compression.clevel,
+            shuffle: value.compression.shuffle,
         })
     }
 }
@@ -93,6 +128,18 @@ impl TryFrom<&StorageProperties> for capi::StorageProperties {
             (null(), 0)
         };
 
+        // Careful: z needs to live long enough
+        let z = if let Some(compression_codec) = &value.compression_codec {
+            Some(CString::new(compression_codec.as_str())?)
+        } else {
+            None
+        };
+        let (codec_id, bytes_of_codec_id) = if let Some(ref z) = z {
+            (z.as_ptr(), z.to_bytes_with_nul().len())
+        } else {
+            (null(), 0)
+        };
+
         // This copies the string into a buffer owned by the return value.
         if !unsafe {
             capi::storage_properties_init(
@@ -106,7 +153,24 @@ impl TryFrom<&StorageProperties> for capi::StorageProperties {
                     x: value.pixel_scale_um.0,
                     y: value.pixel_scale_um.1,
                 },
-                value.bytes_per_chunk,
+            ) == 1
+        } {
+            Err(anyhow::anyhow!("Failed acquire api status check"))
+        } else if !unsafe {
+            capi::storage_properties_set_chunking_props(&mut out,
+                                                        value.tile_width,
+                                                        value.tile_height,
+                                                        value.tile_planes,
+                                                        value.bytes_per_chunk,
+            ) == 1
+        } {
+            Err(anyhow::anyhow!("Failed acquire api status check"))
+        } else if !unsafe {
+            capi::storage_properties_set_compression_props(&mut out,
+                                                           codec_id,
+                                                           bytes_of_codec_id,
+                                                           value.clevel,
+                                                           value.shuffle,
             ) == 1
         } {
             Err(anyhow::anyhow!("Failed acquire api status check"))
@@ -123,7 +187,9 @@ impl Default for capi::StorageProperties {
             first_frame_id: Default::default(),
             external_metadata_json: Default::default(),
             pixel_scale_um: Default::default(),
-            bytes_per_chunk: Default::default(),
+            image_shape: Default::default(),
+            chunking: Default::default(),
+            compression: Default::default(),
         }
     }
 }
@@ -143,6 +209,59 @@ impl Default for capi::PixelScale {
         Self {
             x: Default::default(),
             y: Default::default(),
+        }
+    }
+}
+
+impl Default for capi::StorageProperties_storage_properties_chunking_s {
+    fn default() -> Self {
+        Self {
+            bytes_per_chunk: Default::default(),
+            tile_width: Default::default(),
+            tile_height: Default::default(),
+            tile_planes: Default::default(),
+        }
+    }
+}
+
+impl Default for capi::StorageProperties_storage_properties_compression_s {
+    fn default() -> Self {
+        Self {
+            codec_id: Default::default(),
+            clevel: Default::default(),
+            shuffle: Default::default(),
+        }
+    }
+}
+
+impl Default for capi::ImageShape_image_dims_s {
+    fn default() -> Self {
+        Self {
+            channels: Default::default(),
+            width: Default::default(),
+            height: Default::default(),
+            planes: Default::default(),
+        }
+    }
+}
+
+impl Default for capi::ImageShape_image_strides_s {
+    fn default() -> Self {
+        Self {
+            channels: Default::default(),
+            width: Default::default(),
+            height: Default::default(),
+            planes: Default::default(),
+        }
+    }
+}
+
+impl Default for capi::ImageShape {
+    fn default() -> Self {
+        Self {
+            dims: Default::default(),
+            strides: Default::default(),
+            type_: Default::default(),
         }
     }
 }
