cba1d40b63081a41698e6d205e4a6fb4d98a0509
Alan Liddell <aliddell@chanzuckerberg.com>
2023-05-11 17:30:30 -0400

    Address PR comments

--- build.rs
+++ build.rs
@@ -1,41 +1,4 @@
-use serde::Deserialize;
 use std::io::Cursor;
-
-/// Component of the `Artifact` JSON object returned from the GitHub API.
-#[allow(dead_code)]
-#[derive(Deserialize, Clone)]
-struct WorkFlowRun {
-    id: i128,
-    repository_id: i128,
-    head_repository_id: i128,
-    head_branch: String,
-    head_sha: String,
-}
-
-/// Metadata for a build artifact from given commit to a given repository, returned from GitHub API.
-#[allow(dead_code)]
-#[derive(Deserialize, Clone)]
-struct Artifact {
-    id: i128,
-    node_id: String,
-    name: String,
-    size_in_bytes: i64,
-    url: String,
-    archive_download_url: String,
-    expired: bool,
-    created_at: String,
-    updated_at: String,
-    expires_at: String,
-    workflow_run: WorkFlowRun,
-}
-
-/// List of build artifacts for a given repository, returned from GitHub API.
-#[allow(dead_code)]
-#[derive(Deserialize, Clone)]
-struct ArtifactsResponse {
-    total_count: u32,
-    artifacts: Vec<Artifact>,
-}
 
 fn main() {
     let dst = dbg!(cmake::Config::new("acquire-video-runtime")
@@ -76,9 +39,8 @@ fn main() {
     println!("cargo:rustc-link-lib=static=acquire-device-hal");
     println!("cargo:rustc-link-lib=static=acquire-core-platform");
     println!("cargo:rustc-link-lib=static=acquire-core-logger");
-    if cfg!(target_os = "linux") {
-        println!("cargo:rustc-link-lib=static=stdc++");
-    }
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-lib=static=stdc++");
 
     println!("cargo:rerun-if-changed=wrapper.h");
     // TODO: expand rerun-if-changed so we don't have to touch wrapper so much
@@ -119,11 +81,13 @@ fn fetch_acquire_driver(dst: &std::path::PathBuf, name: &str, tag: &str) {
         .header("X-GitHub-Api-Version", "2022-11-28");
 
     let archive = match request.send() {
-        Ok(r) => r.bytes().unwrap(),
+        Ok(r) => r.bytes(),
         Err(err) => panic!("HTTP request for {} failed, got {}", &name, err),
-    };
+    }.expect(&*format!("Failed to get response body for {} as bytes.", name));
 
-    zip_extract::extract(Cursor::new(archive), &dst, true).unwrap();
+    zip_extract::extract(
+        Cursor::new(archive), &dst, true,
+    ).expect(&*format!("Failed to extract {name}-{tag}-{build}.zip from response."));
 
     copy_acquire_driver(&dst, name);
 }
@@ -145,8 +109,8 @@ fn copy_acquire_driver(dst: &std::path::PathBuf, name: &str) {
         format!("{}/lib/{lib}", dst.display()),
         format!("python/acquire/{lib}"),
     )
-    .expect(&format!(
-        "Failed to copy {}/lib/{lib} to python folder.",
-        dst.display()
-    ));
+        .expect(&format!(
+            "Failed to copy {}/lib/{lib} to python folder.",
+            dst.display()
+        ));
 }
