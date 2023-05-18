932386284f6183e0bf070632ae78483026652baf
Alan Liddell <aliddell@chanzuckerberg.com>
2023-05-18 12:59:47 -0400

    Nightly release (#16)
    * Add drivers config
    
    * build nightly release using nightly dependencies
    
    * add a comment
    
    * fix build and release jobs
    
    * Address PR comments
    
    * Address PR comments
    
    * Address PR comments

--- build.rs
+++ build.rs
@@ -1,4 +1,15 @@
-use std::io::Cursor;
+use std::fs;
+use serde::Deserialize;
+
+/// Struct representation of the manifest file drivers.json
+#[derive(Deserialize)]
+#[serde(rename_all = "kebab-case")]
+struct DriverManifest {
+    acquire_driver_common: String,
+    acquire_driver_zarr: String,
+    acquire_driver_egrabber: String,
+    acquire_driver_hdcam: String,
+}
 
 fn main() {
     let dst = dbg!(cmake::Config::new("acquire-video-runtime")
@@ -11,26 +22,31 @@ fn main() {
         .define("CMAKE_OSX_DEPLOYMENT_TARGET", "10.15")
         .build());
 
+    let drivers_json = fs::read_to_string("drivers.json")
+        .expect("Failed to read from drivers.json.");
+    let tags: DriverManifest = serde_json::from_str(drivers_json.as_str())
+        .expect("Failed to parse drivers.json");
+
     let out = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
     fetch_acquire_driver(
         &out,
         "acquire-driver-common",
-        "0.1.1",
+        tags.acquire_driver_common.as_str(),
     );
     fetch_acquire_driver(
         &out,
         "acquire-driver-zarr",
-        "0.1.1",
+        tags.acquire_driver_zarr.as_str(),
     );
     fetch_acquire_driver(
         &out,
         "acquire-driver-egrabber",
-        "0.1.1",
+        tags.acquire_driver_egrabber.as_str(),
     );
     fetch_acquire_driver(
         &out,
         "acquire-driver-hdcam",
-        "0.1.2",
+        tags.acquire_driver_hdcam.as_str(),
     );
 
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
@@ -74,7 +90,12 @@ fn fetch_acquire_driver(dst: &std::path::PathBuf, name: &str, tag: &str) {
         .build()
         .unwrap();
 
-    let uri = format!("https://github.com/acquire-project/{name}/releases/download/v{tag}/{name}-{tag}-{build}.zip");
+    let vstring = if tag == "nightly" {
+        tag.to_owned()
+    } else {
+        format!("v{tag}")
+    };
+    let uri = format!("https://github.com/acquire-project/{name}/releases/download/{vstring}/{name}-{tag}-{build}.zip");
     let request = client
         .get(uri)
         .header("Accept", "application/vnd.github+json")
@@ -86,7 +107,7 @@ fn fetch_acquire_driver(dst: &std::path::PathBuf, name: &str, tag: &str) {
     }.expect(&*format!("Failed to get response body for {} as bytes.", name));
 
     zip_extract::extract(
-        Cursor::new(archive), &dst, true,
+        std::io::Cursor::new(archive), &dst, true,
     ).expect(&*format!("Failed to extract {name}-{tag}-{build}.zip from response."));
 
     copy_acquire_driver(&dst, name);
