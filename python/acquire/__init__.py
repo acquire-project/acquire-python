c39a655c2032209371247709bf836ad793d194b6
Andy Sweet <andrew.d.sweet@gmail.com>
2023-07-13 14:18:02 -0700

    Fix and support one stream

--- python/acquire/__init__.py
+++ python/acquire/__init__.py
@@ -1,5 +1,7 @@
 import time
-from typing import Any, List, Optional, Tuple, Union
+from typing import TYPE_CHECKING, Any, Generator, List, Literal, Optional, Tuple, Union
+
+import numpy.typing as npt
 
 from . import acquire
 from .acquire import *
@@ -7,6 +9,10 @@ from .acquire import *
 __doc__ = acquire.__doc__
 
 import logging
+
+if TYPE_CHECKING:
+    import napari
+
 
 FORMAT = (
     "%(levelname)s %(name)s %(asctime)-15s %(filename)s:%(lineno)d %(message)s"
@@ -56,11 +62,14 @@ def setup_one_streams(runtime: Runtime, frame_count: int) -> Properties:
     ]
     logging.warning(f"Cameras {cameras}")
 
+    if len(cameras) == 0:
+        cameras = ["simulated.*sin.*"]
+
     p.video[0].camera.identifier = dm.select(DeviceKind.Camera, cameras[0])
     p.video[0].storage.identifier = dm.select(DeviceKind.Storage, "Trash")
     # p.video[0].storage.settings.filename = output_filename
     p.video[0].camera.settings.binning = 1
-    p.video[0].camera.settings.shape = (2304, 2304)
+    p.video[0].camera.settings.shape = (64, 64)
     p.video[0].camera.settings.pixel_type = SampleType.U16
     p.video[0].max_frame_count = frame_count
     p.video[0].frame_average_count = 0  # disables
@@ -102,31 +111,19 @@ def setup_two_streams(runtime: Runtime, frame_count: int) -> Properties:
     return p
 
 
-g_runtime = None
-
-
 def gui(
     viewer: "napari.Viewer",
+    stream_count: Literal[1, 2] = 1,
     frame_count: int = 100,
 ):
     """Napari dock-widget plugin entry-point
 
     This instances a magicgui dock widget that streams video to a layer.
     """
-    import numpy.typing as npt
     from napari.qt.threading import thread_worker
     from numpy import cumsum, histogram, where
 
     update_times: List[float] = []
-
-    def get_runtime():
-        global g_runtime
-        if g_runtime is None:
-            logging.info("INITING RUNTIME")
-            g_runtime = acquire.Runtime()
-        else:
-            logging.info("REUSING RUNTIME")
-        return g_runtime
 
     def update_layer(args: Tuple[npt.NDArray[Any], int]):
         (new_image, stream_id) = args
@@ -147,13 +144,16 @@ def gui(
             viewer.add_image(new_image, name=layer_key)
 
     @thread_worker(connect={"yielded": update_layer})
-    def do_acquisition():
+    def do_acquisition() -> Generator[Tuple[npt.NDArray[Any], int], None, None]:
         logging.basicConfig(level=logging.DEBUG)
         logging.getLogger().setLevel(logging.DEBUG)
 
-        runtime = get_runtime()
-        # p = setup_two_streams(runtime,frame_count)
-        p = setup_two_streams(runtime, frame_count)
+        runtime = _get_runtime()
+
+        if stream_count == 1:
+            p = setup_one_streams(runtime, frame_count)
+        else:
+            p = setup_two_streams(runtime, frame_count)
 
         p = runtime.set_configuration(p)
 
@@ -171,21 +171,9 @@ def gui(
             clock = time.time()
 
             if nframes[stream_id] < p.video[stream_id].max_frame_count:
-
-                if packet := runtime.get_available_data(stream_id):
-                    n = packet.get_frame_count()
-                    f = next(packet.frames())
-                    im = f.data().squeeze().copy()
-                    logging.debug(
-                        f"stream {stream_id} frame {f.metadata().frame_id}"
-                    )
-
-                    # TODO: (nclack) fix this awkwardness.
-                    f = None
-                    packet = None
-
+                if frame := _next_frame(runtime, stream_id):
+                    im, n = frame
                     yield (im, stream_id)
-
                     nframes[stream_id] += n
                     logging.info(
                         f"[stream {stream_id}] frame count: {nframes}"
@@ -204,7 +192,33 @@ def gui(
         runtime.stop()
         logging.info("STOPPED")
 
+    viewer.layers.clear()
     do_acquisition()
+
+
+_g_runtime: Optional[Runtime] = None
+"""The global acquire runtime."""
+
+
+def _get_runtime() -> Runtime:
+    """Potentially create and get the global acquire runtime."""
+    global _g_runtime
+    if _g_runtime is None:
+        logging.info("INITING RUNTIME")
+        _g_runtime = acquire.Runtime()
+    else:
+        logging.info("REUSING RUNTIME")
+    return _g_runtime
+
+
+def _next_frame(runtime: Runtime, stream_id: int) -> Optional[Tuple[npt.NDArray[Any], int]]:
+    """Get the next frame from the given stream with its count."""
+    if packet := runtime.get_available_data(stream_id):
+        n = packet.get_frame_count()
+        f = next(packet.frames())
+        logging.debug(f"stream {stream_id} frame {f.metadata().frame_id}")
+        return f.data().squeeze().copy(), n
+    return None
 
 
 # TODO: (nclack) add context manager around runtime and start/stop
