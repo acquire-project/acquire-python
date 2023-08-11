66a404353f2ceefd5853cd3d6a090b4a1aaa6c86
Nathan Clack <nclack@gmail.com>
2023-08-10 17:35:35 -0700

    Update to acquire-driver-hdcam v0.1.6 and add test

--- tests/test_dcam.py
+++ tests/test_dcam.py
@@ -1,5 +1,3 @@
-import logging
-
 import acquire
 import pytest
 from acquire import DeviceKind, SampleType
@@ -15,28 +13,44 @@ def test_ext_triggering(runtime: acquire.Runtime):
     dm = runtime.device_manager()
     p = runtime.get_configuration()
 
-    cameras = [
-        d.name
-        for d in dm.devices()
-        if (d.kind == DeviceKind.Camera) and ("C15440" in d.name)
-    ]
-    assert len(cameras) > 0, "No C15440 cameras found"
-    logging.warning(f"Cameras {cameras}")
-
-    p.video[0].camera.identifier = dm.select(DeviceKind.Camera, cameras[0])
+    p.video[0].camera.identifier = dm.select(
+        DeviceKind.Camera, "Hamamatsu C15440-20UP.*"
+    )
     p.video[0].storage.identifier = dm.select(DeviceKind.Storage, "Trash")
     p.video[0].camera.settings.binning = 1
     p.video[0].camera.settings.shape = (2304, 2304)
     p.video[0].camera.settings.pixel_type = SampleType.U16
-    p.video[0].max_frame_count = 1
-    p.video[0].frame_average_count = 0  # disables
+    p.video[0].max_frame_count = 10
 
-    # Set the camera here so we can query it's triggering capabilities.
-    # This comes in the form of the returned properties.
+    # line=0 corresponds with "Ext.Trig"
     p.video[0].camera.settings.input_triggers.frame_start = acquire.Trigger(
         enable=True, line=0, edge="Rising"
     )
-    # Call set_configuration() again to apply the trigger properties
+
+    # Apply configuration and read back the configuration from devices
     p = runtime.set_configuration(p)
     # Check that it was accepted
     assert p.video[0].camera.settings.input_triggers.frame_start.enable is True
+
+
+def test_set_exposure_output_trigger(runtime: acquire.Runtime) -> None:
+    dm = runtime.device_manager()
+    p = runtime.get_configuration()
+
+    p.video[0].camera.identifier = dm.select(
+        DeviceKind.Camera, "Hamamatsu C15440-20UP.*"
+    )
+    p.video[0].storage.identifier = dm.select(DeviceKind.Storage, "Trash")
+    p.video[0].camera.settings.binning = 1
+    p.video[0].camera.settings.shape = (2304, 2304)
+    p.video[0].camera.settings.pixel_type = SampleType.U16
+    p.video[0].max_frame_count = 10
+
+    # line=1 corresponds with "Timing 1"
+    p.video[0].camera.settings.output_triggers.exposure = acquire.Trigger(
+        enable=True, line=1, edge="Rising"
+    )
+    # Apply configuration and read back the configuration from devices
+    p = runtime.set_configuration(p)
+    # Check that it was accepted
+    assert p.video[0].camera.settings.output_triggers.exposure.enable is True
