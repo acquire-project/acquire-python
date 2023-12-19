031a34796cb52da784c6e97df5fb14a488cc4420
Alan Liddell <aliddell@chanzuckerberg.com>
2023-12-19 16:42:57 -0500

    Set exposure time and sleep a little bit longer in test_setup.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -163,6 +163,7 @@ def test_setup(runtime: Runtime):
     assert p.video[0].storage.settings.filename == "out.tif"
     assert p.video[0].max_frame_count == 100
     p.video[0].camera.settings.pixel_type = acquire.SampleType.U8
+    p.video[0].camera.settings.exposure_time_us = 2e3
     p.video[0].camera.settings.shape = (192, 108)
     p = runtime.set_configuration(p)
 
@@ -192,7 +193,7 @@ def test_setup(runtime: Runtime):
             )
 
         elapsed = time.time() - clock
-        sleep(max(0, 0.1 - elapsed))
+        sleep(max(0, 0.4 - elapsed))
     logging.info("stopping")
 
     runtime.stop()
