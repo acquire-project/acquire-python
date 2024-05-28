526b2e01952711743dd82e05c389d86e483617fe
Alan Liddell <aliddell@chanzuckerberg.com>
2024-05-28 12:34:10 -0400

    function scope the runtime fixture in egrabber tests

--- tests/test_egrabber.py
+++ tests/test_egrabber.py
@@ -7,16 +7,10 @@ from acquire import DeviceKind, SampleType
 from acquire.acquire import Trigger
 
 
-@pytest.fixture(scope="module")
-def _runtime():
+@pytest.fixture(scope="function")
+def runtime():
     runtime = acquire.Runtime()
     yield runtime
-
-
-@pytest.fixture(scope="function")
-def runtime(_runtime: acquire.Runtime):
-    yield _runtime
-    _runtime.set_configuration(acquire.Properties())
 
 
 def test_vieworks_camera_is_present(runtime: acquire.Runtime):
