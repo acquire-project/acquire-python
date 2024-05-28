07326a23e65124787e615fdab564b0e40d698e0a
Talley Lambert <talley.lambert@gmail.com>
2024-05-28 10:19:47 -0400

    update

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,12 +1,11 @@
+import atexit
 from pathlib import Path
+import shutil
 import subprocess
 import re
 import logging
+import tempfile
 import pytest
-import os
-
-if not os.getenv("CI") or os.getenv("SKIP_DOCS_TEST", False):
-    pytest.skip("Skipping docs test", allow_module_level=True)
 
 
 logging.getLogger("acquire").setLevel(logging.CRITICAL)
@@ -18,17 +17,26 @@ SKIP = {
     "trigger.md",  # has some non-existant paths
 }
 
-# NOTE: this clones the repo on import... not the best practice, could be improved
-if not (DOCS_PATH := Path("acquire-docs", "docs")).exists():
-    subprocess.check_call(["git", "clone", DOCS_REPO])
+
+def tutorials():
+    tmp_path = Path(tempfile.mkdtemp())
+    subprocess.check_call(["git", "clone", DOCS_REPO], cwd=str(tmp_path))
+    docs_path = tmp_path / "acquire-docs" / "docs"
+
+    tuts = []
+    if (get_started := docs_path / "get_started.md").exists():
+        tuts.append(get_started)
+    tuts.extend([fn for fn in docs_path.glob("tutorials/*.md") if fn.name not in SKIP])
+    tuts.sort()
+
+    @atexit.register
+    def cleanup():
+        shutil.rmtree(tmp_path, ignore_errors=True)
+
+    return tuts
 
 
-TUTS = [DOCS_PATH / "get_started.md"]
-TUTS.extend([fn for fn in DOCS_PATH.glob("tutorials/*.md") if fn.name not in SKIP])
-TUTS.sort()
-
-
-@pytest.mark.parametrize("tutorial", TUTS, ids=lambda x: x.name)
+@pytest.mark.parametrize("tutorial", tutorials(), ids=lambda x: x.name)
 def test_tutorials(tutorial: Path):
     for code_block in CODE_BLOCK.finditer(tutorial.read_text()):
         exec(code_block.group(1))
